2017477f278f984ad52c9bfe80a883d2377e43c6
Adriano Sanches <adrianomsancho@gmail.com>
2025-05-15 13:50:48 +0200

    settings19

--- snakes_and_ladders/game/urls.py
+++ snakes_and_ladders/game/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from .views import StartGame, GameState, RollDice, RestartGame
+from django.http import JsonResponse
+from django.views import View
+
+class TestView(View):
+    def get(self, request):
+        return JsonResponse({"message": "Test view works!"})
 
 urlpatterns = [
     path('start/', StartGame.as_view()),
     path('state/', GameState.as_view()),
-    path('api/roll/', RollDice.as_view()),
+    path('roll/', RollDice.as_view()),
     path('restart/', RestartGame.as_view()),
+    path('test/', TestView.as_view()),
 ]
