75c6ab86b8b1846470806eb032bc0f6331fcc0f8
Adriano Sanches <adrianomsancho@gmail.com>
2025-05-15 12:04:16 +0200

    settings11

--- snakes_and_ladders/game/urls.py
+++ snakes_and_ladders/game/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import StartGame, GameState, RollDice, RestartGame
 
 urlpatterns = [
-    path('api/start/', StartGame.as_view()),
-    path('api/state/', GameState.as_view()),
-    path('api/roll/', RollDice.as_view()),
-    path('api/restart/', RestartGame.as_view()),
+    path('start/', StartGame.as_view()),
+    path('state/', GameState.as_view()),
+    path('roll/', RollDice.as_view()),
+    path('restart/', RestartGame.as_view()),
 ]
