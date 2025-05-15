53be72a910e40ff75e3ade7db5062398b8de4734
Adriano Sanches <adrianomsancho@gmail.com>
2025-05-15 13:58:18 +0200

    settings20

--- snakes_and_ladders/game/urls.py
+++ snakes_and_ladders/game/urls.py
@@ -7,10 +7,14 @@ class TestView(View):
     def get(self, request):
         return JsonResponse({"message": "Test view works!"})
 
+def root_view(request):
+    return JsonResponse({"message": "Root URL works"})
+
 urlpatterns = [
     path('start/', StartGame.as_view()),
     path('state/', GameState.as_view()),
     path('roll/', RollDice.as_view()),
     path('restart/', RestartGame.as_view()),
     path('test/', TestView.as_view()),
+    path('', root_view),
 ]
